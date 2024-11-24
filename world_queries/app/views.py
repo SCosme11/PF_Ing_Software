b0f3f2eff4b57eafe990df3a22f937d0e51cd148
CosmiChala <64669678+CosmiChala@users.noreply.github.com>
2024-11-24 09:48:05 -0600

    Update views.py

--- world_queries/app/views.py
+++ world_queries/app/views.py
@@ -2,7 +2,13 @@ from flask import render_template
 from app import db
 from app.models import Country, City, CountryLanguage
 from app.queries import get_complex_query_1, get_complex_query_2
+from .queries import consulta_poblacion_promedio
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/consulta1')
+def consulta_1():
+    resultados = consulta_poblacion_promedio()
+    return render_template('consulta_1.html', resultados=resultados)
