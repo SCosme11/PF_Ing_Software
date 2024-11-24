5dddd817b8353869ad2e58f3352a203cb5fb0a53
SCosme11 <117309490+SCosme11@users.noreply.github.com>
2024-11-24 09:32:56 -0600

    Update __init__.py

---
+++ world_queries/app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+
+#Instancia de SQLAlchemy
+db = SQLAlchemy()
+
+def create_app():
+    #Crear la aplicación Flask
+    app = Flask(__name__)
+
+    #Carga la configuración desde config.py
+    app.config.from_object(Config)
+
+    #Inicializa la db
+    db.init_app(app)
+
+    return app
